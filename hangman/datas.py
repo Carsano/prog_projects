d8ce80827d24b78d1bce92cfcc037039810b5e75
Carsano <damien.fabiano@gadz.org>
2020-07-19 18:36:29 +0200

    import first draft of hangman

---
+++ hangman/datas.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.8
+
+# -*-coding:UTF-8 -*
+
+word_dictionnary = ["test"]
+lifes=8
+score=0
---
+++ hangman/fonctions.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3.8
+
+# -*-coding:UTF-8 -*
+from datas import *
+
+import random
+
+
+def generation_word():
+	# Va chercher dans le fichier données un mot aléatoire
+	#return le mot
+	word = random.choice(word_dictionnary)
+	return word
+	# validée
+
+def word_validation(word):
+	datas.word = word
+
+
+def letter_check(letter,word=word):
+	# check la lettre dans le mot
+	# return True si lettre dans le mot, False sinon
+	if letter in word.split():
+		return letter, True
+	else:
+		return letter, False
+
+def ask_letter():
+	# demande à l'utilisateur de rentrer une lettre
+	# check si la lettre peut être rentrée
+	# return la lettre
+	letter = input("Please choose a letter: ")
+	can_paly = False
+	while can_play == False:
+		can_play = can_play_letter(letter)
+	return letter
+
+def can_play_letter(letter):
+	# vérifie que la lettre est bien une lettre
+	# vérifie que la lettre n'a pas déjà été jouée
+	# return true ou false
+	if not letter.isalpha():
+		print("This is not a playable character")
+		return False
+	elif letter in false_letters or letter in found_letters:
+		print("You've already played this letter")
+		return False
+	else:
+		return True
+
+def print_false_letters():
+	# affiche la liste des lettres_fausses
+	print(false_letters)
+
+def send_letter_good_false(letter, boolean):
+	# envoie la lettre dans la liste bonne ou dans la liste fausse
+	if boolean:
+		found_letters.append(letter)
+	else:
+		found_letters.append(letter)
+
+
+#def nom_joueur():
+	# demande le nom du joueur 
+	# enregistre ce nom dans donnes
+
+#def enregistrer_score():
+	# ouvre ou crée fichier scores
+	# enregistre son nom avec le score 0
+
+def lose_life(lives=lives):
+	lives -= 1
+	return lives
+
+
+#def tour():
+	# joue un tour de pendu
+	# affiche le mot caché
+	# demande au joueur de taper une lettre
+	# vérifie la lettre
+	# affiche si le joueur a trouvé une lettre ou pas
+	# enlève une vie si besoin
+
+
+word_validation(generation_word())
+print(word)
